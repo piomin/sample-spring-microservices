b93b4702a3e506f6b9f81f017509390f19eabace
Piotr Mińkowski <piotr_minkowski@wp.pl>
2017-02-20 00:08:54 +0100

    Fixes

--- gateway-service/src/main/java/pl/piomin/microservices/edge/WebSecurity.java
+++ gateway-service/src/main/java/pl/piomin/microservices/edge/WebSecurity.java
@@ -3,16 +3,26 @@ package pl.piomin.microservices.edge;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Configuration
+@SessionAttributes("authorizationRequest")
 public class WebSecurity extends WebSecurityConfigurerAdapter   {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    http.antMatcher("/**")
-        .authorizeRequests()
-        .anyRequest()
-        .authenticated();
+		http.csrf().disable();
+		super.configure(http);
+//	    http.antMatcher("/**")
+//        .authorizeRequests()
+//        .anyRequest().permitAll();
+//        .authenticated();
+//		http.authorizeRequests().anyRequest().authenticated()
+//        .and()
+//        .httpBasic().and().csrf().disable();
+//		http.csrf().disable();
 	}
 	
 }
