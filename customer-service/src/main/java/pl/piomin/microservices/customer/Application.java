e36fec41e6d17410624c60b9d40b0de742d41737
piotr.minkowski <piotr.minkowski@gmail.com>
2017-03-29 17:18:30 +0200

    Fixes for working sample

--- customer-service/src/main/java/pl/piomin/microservices/customer/Application.java
+++ customer-service/src/main/java/pl/piomin/microservices/customer/Application.java
@@ -2,11 +2,9 @@ package pl.piomin.microservices.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
 @EnableFeignClients
 public class Application {
 
