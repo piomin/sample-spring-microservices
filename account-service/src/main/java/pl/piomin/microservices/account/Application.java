74021aa026a9ae2ad60a218281ceb9ebc0a35b26
Piotr Mińkowski <piotr_minkowski@wp.pl>
2017-05-12 00:03:01 +0200

    enable hystrix

--- account-service/src/main/java/pl/piomin/microservices/account/Application.java
+++ account-service/src/main/java/pl/piomin/microservices/account/Application.java
@@ -2,10 +2,14 @@ package pl.piomin.microservices.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class Application {
 
 	public static void main(String[] args) {
--- customer-service/src/main/java/pl/piomin/microservices/customer/Application.java
+++ customer-service/src/main/java/pl/piomin/microservices/customer/Application.java
@@ -2,12 +2,14 @@ package pl.piomin.microservices.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class Application {
 
 	public static void main(String[] args) {
