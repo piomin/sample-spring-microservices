cfb0603dcee9a84c00a6c4a228178e8e8f167a75
Ramazan Sakin <ramazansakin63@gmail.com>
2021-09-24 14:55:13 +0300

    feat: migrating the project java11 & spring.boot-2.1.5.RELEASE

--- zipkin-service/src/main/java/pl/piomin/microservices/zipkin/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/pl/piomin/microservices/zipkin/ZipkinServiceApplication.java
@@ -2,15 +2,14 @@ package pl.piomin.microservices.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
 public class ZipkinServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServiceApplication.class, args);
+    }
 
 }
